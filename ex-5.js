655db87d6aeaf39c449b686cb0c93a70e4bf8b23
Arisa Teandum <basarisa.1302@gmail.com>
2024-10-04 16:04:29 +0700

    feat: loop assignment

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,8 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
 
+for (let num = 0; num < companyName.length; num++) {
+  console.log(`Number ${num + 1} character is ${companyName[num]}`);
+}
+
 // Start coding here
